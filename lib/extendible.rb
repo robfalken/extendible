5e74ee8758b17e3b1e7abb44139fcf773ce11bde
Robert Falkén <robert@robertfalken.se>
2014-03-27 16:43:12 +0100

    use custom serializer if existent

--- lib/extendible.rb
+++ lib/extendible.rb
@@ -15,7 +15,7 @@ module Extendible
     scope[:extend].split(',').map(&:strip).each do |object_name|
       if object_name.include?('.') # dot found, split into object and attribute
         obj, attr = object_name.split('.')
-        all[obj.to_sym] = ['ids'] unless all.has_key?(obj.to_sym)
+        all[obj.to_sym] = ['id'] unless all.has_key?(obj.to_sym)
         if all.has_key?(obj.to_sym) # object already has attributes specified, add attribute to array
           all[obj.to_sym] << attr
         else # first attribute for array, initialize array with attribute
@@ -29,21 +29,26 @@ module Extendible
     @extended_objects = all
   end
 
+  def serialize(object, child_sym)
+    serializer_name = "#{child_sym.to_s.camelize}Serializer"              
+    serializer_name.constantize.new(object.try(child_sym)) rescue object.try(child_sym)
+  end
+
   module ClassMethods
     def extendible(*children)
       # iterate through all obects that are supposed to be extendible
       children.each do |child_sym|
         attributes child_sym
         define_method child_sym do
-          puts "\033[42m ### in define_method \033[0m"
           # nil if child is nonexistent
           return nil if object.try(child_sym).nil?
           if extended_objects.has_key?(child_sym) # params[:extend] includes child, extend attributes
             if extended_objects[child_sym].nil? # no attributes are specified, extend with all
-              puts "\033[42m ### using CustomerSerializer \033[0m"
-              CustomerSerializer.new(object.try(child_sym)).as_json
+              serialize(object, child_sym).as_json
             else # only extend with attributes provided with dot notation
-              object.try(child_sym).attributes.extract!(*extended_objects[child_sym].map(&:to_s))
+              serialized_object = serialize(object, child_sym)
+              mapper = (serialized_object.class < ActiveModel::Serializer) ? :to_sym : :to_s
+              serialized_object.attributes.extract!(*extended_objects[child_sym].map(&mapper))            
             end
           else # child is not in params[:extend], only include id
             object.try(child_sym).attributes.extract!('id')
@@ -51,6 +56,7 @@ module Extendible
         end
       end
     end
+
   end
 
 end
